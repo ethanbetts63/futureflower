23e4fcd28e0f75fcf38d0438dcb410973d449b19
Ethan Betts <you@example.com>
2026-01-21 16:57:17 +0100

    checkpoint

--- frontend/src/pages/user_dashboard/UserDashboardLayout.tsx
+++ frontend/src/pages/user_dashboard/UserDashboardLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 function UserDashboardLayout() {
   return (
-    <div className="flex min-h-screen w-full flex-col">
+    <div className="flex min-h-screen w-full flex-col" style={{ backgroundColor: 'var(--color4)' }}>
       <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
         <Outlet />
       </main>
