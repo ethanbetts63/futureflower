3a7997c6bb4144463d4ffe980b4c7a77f248e1ca
Ethan Betts <you@example.com>
2026-02-17 22:33:43 +0100

    reset_django.ps1

--- events/migrations/0003_initial.py
+++ events/migrations/0003_initial.py
@@ -10,18 +10,29 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ("payments", "0001_initial"),
+        ("events", "0002_initial"),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
         migrations.AddField(
-            model_name="payment",
+            model_name="orderbase",
             name="user",
             field=models.ForeignKey(
+                help_text="The user who owns this order.",
                 on_delete=django.db.models.deletion.CASCADE,
-                related_name="payments",
+                related_name="orders",
                 to=settings.AUTH_USER_MODEL,
+            ),
+        ),
+        migrations.AddField(
+            model_name="event",
+            name="order",
+            field=models.ForeignKey(
+                help_text="The order this event belongs to.",
+                on_delete=django.db.models.deletion.CASCADE,
+                related_name="events",
+                to="events.orderbase",
             ),
         ),
     ]
